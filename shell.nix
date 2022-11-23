71de017ddca7da473938cb79920b9caef4e828f8
Jim Newsome <jnewsome@torproject.org>
2022-11-22 22:21:52 -0600

    initial shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+let
+  sources = import ./nix/sources.nix { };
+  pkgs = import sources.nixpkgs { };
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    coreutils
+    git
+    rustup
+    vim
+  ];
+}
