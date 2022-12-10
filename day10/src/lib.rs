fba4f933217d3fdbafe6657ea9f7fb363d6237c6
Jim Newsome <jnewsome@torproject.org>
2022-12-10 08:07:47 -0600

    d10p1

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,14 +1,223 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::str::FromStr;
+
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+enum Instruction {
+    Nop,
+    Add(i64),
 }
 
-#[cfg(test)]
-mod tests {
+impl FromStr for Instruction {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut s = s.split_whitespace();
+        let t1 = s.next().unwrap();
+        match t1 {
+            "addx" => {
+                let i: i64 = s.next().unwrap().parse().unwrap();
+                Ok(Instruction::Add(i))
+            }
+            "noop" => Ok(Instruction::Nop),
+            _ => panic!("Bad token {t1}"),
+        }
+    }
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+struct State {
+    x: i64,
+    cycles_executed: usize,
+}
+
+fn execute(state: State, inst: Instruction) -> State {
+    match inst {
+        Instruction::Nop => State {
+            x: state.x,
+            cycles_executed: state.cycles_executed + 1,
+        },
+        Instruction::Add(i) => State {
+            x: state.x + i,
+            cycles_executed: state.cycles_executed + 2,
+        },
+    }
+}
+
+pub mod p1 {
     use super::*;
 
+    pub fn solve(input: &str) -> i64 {
+        let instructions = input
+            .lines()
+            .map(|l| l.trim().parse::<Instruction>().unwrap());
+        let mut pending_cycle_counts = vec![220, 180, 140, 100, 60, 20];
+        let mut sum = 0;
+        let mut state = State {
+            x: 1,
+            cycles_executed: 0,
+        };
+        for inst in instructions {
+            let next_state = execute(state, inst);
+            let next_cycle_count = pending_cycle_counts[pending_cycle_counts.len() - 1];
+            if next_state.cycles_executed >= next_cycle_count {
+                sum += i64::try_from(next_cycle_count).unwrap() * state.x;
+                pending_cycle_counts.pop();
+                if pending_cycle_counts.is_empty() {
+                    break;
+                }
+            }
+            state = next_state;
+        }
+        sum
+    }
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_solve() {
+        let input = "addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+        assert_eq!(solve(input), 13140);
     }
 }
--- day9/src/lib.rs
+++ day9/src/lib.rs
@@ -23,6 +23,7 @@ impl FromStr for Dir {
     }
 }
 
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
 struct Instruction {
     dir: Dir,
     count: i32,
@@ -40,46 +41,46 @@ impl FromStr for Instruction {
     }
 }
 
+fn head_pos(prev: (i32, i32), dir: Dir) -> (i32, i32) {
+    match dir {
+        Dir::Up => (prev.0, prev.1 + 1),
+        Dir::Down => (prev.0, prev.1 - 1),
+        Dir::Left => (prev.0 - 1, prev.1),
+        Dir::Right => (prev.0 + 1, prev.1),
+    }
+}
+#[test]
+fn test_head_pos() {
+    assert_eq!(head_pos((0, 0), Dir::Left), (-1, 0));
+    assert_eq!(head_pos((0, 0), Dir::Right), (1, 0));
+    assert_eq!(head_pos((0, 0), Dir::Up), (0, 1));
+    assert_eq!(head_pos((0, 0), Dir::Down), (0, -1));
+}
+
+fn tail_pos(tail: (i32, i32), head: (i32, i32)) -> (i32, i32) {
+    let diff = (head.0 - tail.0, head.1 - tail.1);
+    let diff_sign = (diff.0.signum(), diff.1.signum());
+    let diff_mag = (diff.0.abs(), diff.1.abs());
+    assert!(diff_mag.0 <= 2);
+    assert!(diff_mag.1 <= 2);
+    if diff_mag.0 < 2 && diff_mag.1 < 2 {
+        (tail.0, tail.1)
+    } else {
+        (tail.0 + diff_sign.0, tail.1 + diff_sign.1)
+    }
+}
+#[test]
+fn test_tail_pos() {
+    assert_eq!(tail_pos((0, 0), (0, 0)), (0, 0));
+    assert_eq!(tail_pos((0, 0), (0, 2)), (0, 1));
+    assert_eq!(tail_pos((0, 0), (0, -2)), (0, -1));
+    assert_eq!(tail_pos((0, 0), (1, 1)), (0, 0));
+    assert_eq!(tail_pos((0, 0), (2, 2)), (1, 1));
+    assert_eq!(tail_pos((0, 0), (1, 2)), (1, 1));
+}
+
 pub mod p1 {
     use std::collections::BTreeSet;
-
-    fn head_pos(prev: (i32, i32), dir: Dir) -> (i32, i32) {
-        match dir {
-            Dir::Up => (prev.0, prev.1 + 1),
-            Dir::Down => (prev.0, prev.1 - 1),
-            Dir::Left => (prev.0 - 1, prev.1),
-            Dir::Right => (prev.0 + 1, prev.1),
-        }
-    }
-    #[test]
-    fn test_head_pos() {
-        assert_eq!(head_pos((0, 0), Dir::Left), (-1, 0));
-        assert_eq!(head_pos((0, 0), Dir::Right), (1, 0));
-        assert_eq!(head_pos((0, 0), Dir::Up), (0, 1));
-        assert_eq!(head_pos((0, 0), Dir::Down), (0, -1));
-    }
-
-    fn tail_pos(tail: (i32, i32), head: (i32, i32)) -> (i32, i32) {
-        let diff = (head.0 - tail.0, head.1 - tail.1);
-        let diff_sign = (diff.0.signum(), diff.1.signum());
-        let diff_mag = (diff.0.abs(), diff.1.abs());
-        assert!(diff_mag.0 <= 2);
-        assert!(diff_mag.1 <= 2);
-        if diff_mag.0 < 2 && diff_mag.1 < 2 {
-            (tail.0, tail.1)
-        } else {
-            (tail.0 + diff_sign.0, tail.1 + diff_sign.1)
-        }
-    }
-    #[test]
-    fn test_tail_pos() {
-        assert_eq!(tail_pos((0, 0), (0, 0)), (0, 0));
-        assert_eq!(tail_pos((0, 0), (0, 2)), (0, 1));
-        assert_eq!(tail_pos((0, 0), (0, -2)), (0, -1));
-        assert_eq!(tail_pos((0, 0), (1, 1)), (0, 0));
-        assert_eq!(tail_pos((0, 0), (2, 2)), (1, 1));
-        assert_eq!(tail_pos((0, 0), (1, 2)), (1, 1));
-    }
 
     use super::*;
     pub fn solve(input: &str) -> usize {
@@ -111,5 +112,42 @@ pub mod p1 {
         L 5
         R 2";
         assert_eq!(solve(input), 13);
+    }
+}
+
+pub mod p2 {
+    use std::collections::BTreeSet;
+
+    use super::*;
+    pub fn solve(input: &str) -> usize {
+        let instructions = input
+            .lines()
+            .map(|l| -> Instruction { l.trim().parse().unwrap() });
+        let mut knots = [(0i32, 0i32); 10];
+        let mut positions = BTreeSet::<(i32, i32)>::new();
+        positions.insert(knots[9]);
+        for i in instructions {
+            for _ in 0..i.count {
+                knots[0] = head_pos(knots[0], i.dir);
+                for i in 1..knots.len() {
+                    knots[i] = tail_pos(knots[i], knots[i-1]);
+                }
+                positions.insert(knots[9]);
+            }
+        }
+        positions.len()
+    }
+
+    #[test]
+    fn test_solve() {
+        let input = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
+        assert_eq!(solve(input), 36);
     }
 }
