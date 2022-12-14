bd4b9ca279d183c81334220339480f5c01bf6790
Jim Newsome <jnewsome@torproject.org>
2022-12-14 09:34:06 -0600

    refactor for d14 p2

--- day14/src/lib.rs
+++ day14/src/lib.rs
@@ -1,5 +1,5 @@
+use std::collections::HashMap;
 use std::fmt::Display;
-use std::ops::{Index, IndexMut};
 use std::str::FromStr;
 
 // Signed here makes arithmetic simpler
@@ -15,44 +15,35 @@ enum Cell {
     DeadSand,
 }
 
-struct Cave {
-    minx: usize,
-    width: usize,
-    height: usize,
-    cells: Vec<Cell>,
+struct Cave<const FLOORED: bool> {
+    max_wall_y: isize,
+    cells: HashMap<Point, Cell>,
 }
 
-impl Cave {
-    fn idx(&self, point: Point) -> Option<usize> {
-        let x: usize = point.0.try_into().ok()?;
-        let y: usize = point.1.try_into().ok()?;
-        if x >= self.width {
-            None
-        } else if y >= self.height {
-            None
+impl<const FLOORED: bool> Cave<FLOORED> {
+    fn get(&self, point: Point) -> Cell {
+        if FLOORED && point.1 == (self.max_wall_y + 2) {
+            Cell::Rock
         } else {
-            Some(x + y * self.width)
+            *self.cells.get(&point).unwrap_or(&Cell::Empty)
         }
     }
 
-    fn get(&self, point: Point) -> Option<&Cell> {
-        let i = self.idx(point)?;
-        Some(&self.cells[i])
+    fn in_abyss(&self, point: Point) -> bool {
+        !FLOORED && point.1 > self.max_wall_y
     }
 
-    fn get_mut(&mut self, point: Point) -> Option<&mut Cell> {
-        let i = self.idx(point)?;
-        Some(&mut self.cells[i])
+    fn set(&mut self, point: Point, cell: Cell) {
+        self.cells.insert(point, cell);
     }
 
     // Returns final position of the new sand.
     fn process_one_sand(&mut self) -> Option<Point> {
         let mut pos = GENERATOR;
         loop {
-            if self.idx(pos).is_none() {
-                // Off into the abyss
-                return None;
-            };
+            if self.in_abyss(pos) {
+                return None
+            }
             let next = [
                 (pos.0, pos.1 + 1),
                 (pos.0 - 1, pos.1 + 1),
@@ -60,19 +51,15 @@ impl Cave {
             ]
             .iter()
             .copied()
-            .find(|p| {
-                let Some(cell) = self.get(*p) else {
-                    // We can place it here... in the abyss.
-                    return true;
-                };
-                *cell == Cell::Empty
-            });
+            .find(|p| 
+                self.get(*p) == Cell::Empty
+            );
             match next {
                 Some(p) => {
                     pos = p;
                 }
                 None => {
-                    self[pos] = Cell::DeadSand;
+                    self.set(pos, Cell::DeadSand);
                     return Some(pos);
                 }
             }
@@ -80,81 +67,64 @@ impl Cave {
     }
 }
 
-impl Index<Point> for Cave {
-    type Output = Cell;
-
-    fn index(&self, index: Point) -> &Self::Output {
-        self.get(index).unwrap()
-    }
-}
-
-impl IndexMut<Point> for Cave {
-    fn index_mut(&mut self, index: Point) -> &mut Self::Output {
-        self.get_mut(index).unwrap()
-    }
-}
-
-impl FromStr for Cave {
+impl<const FLOORED: bool> FromStr for Cave<FLOORED> {
     type Err = ();
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let paths: Vec<Path> = s.lines().map(|l| l.parse().unwrap()).collect();
         let mut cave = {
             let points: Vec<Point> = paths.clone().into_iter().flatten().collect();
-            let width: usize = points
+            let max_wall_y: isize = points
                 .iter()
-                .map(|p| usize::try_from(p.0).unwrap())
+                .map(|p| p.1)
                 .max()
-                .unwrap()
-                + 1;
-            let height: usize = points
-                .iter()
-                .map(|p| usize::try_from(p.1).unwrap())
-                .max()
-                .unwrap()
-                + 1;
-            let minx: usize = points
-                .iter()
-                .map(|p| usize::try_from(p.0).unwrap())
-                .min()
                 .unwrap();
-            let mut cells: Vec<Cell> = Vec::new();
-            cells.resize(width * height, Cell::Empty);
+            let cells: HashMap<Point, Cell> = HashMap::new();
             Cave {
-                minx,
-                width,
-                height,
+                max_wall_y,
                 cells,
             }
         };
         for path in paths {
             let mut points = path.into_iter();
             let mut current = points.next().unwrap();
-            *cave.get_mut(current).unwrap() = Cell::Rock;
+            cave.set(current, Cell::Rock);
             for dst in points {
                 while dst != current {
                     let dx = (dst.0 - current.0).signum();
                     let dy = (dst.1 - current.1).signum();
                     current = (current.0 + dx, current.1 + dy);
-                    cave[current] = Cell::Rock;
+                    cave.set(current, Cell::Rock);
                 }
             }
         }
-        cave[GENERATOR] = Cell::SandSource;
+        cave.set(GENERATOR, Cell::SandSource);
         Ok(cave)
     }
 }
 
-impl Display for Cave {
+impl<const FLOORED: bool> Display for Cave<FLOORED> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for y in 0..self.height {
+        let maxy = if FLOORED {
+            self.max_wall_y + 2
+        } else {
+            self.max_wall_y
+        };
+        let minx: isize = self.cells.keys()
+            .map(|p| p.0)
+            .min()
+            .unwrap();
+        let maxx: isize = self.cells.keys()
+            .map(|p| p.0)
+            .max()
+            .unwrap();
+        for y in 0..=maxy {
             let y: isize = y.try_into().unwrap();
             if y != 0 {
                 write!(f, "\n")?;
             }
-            for x in self.minx..self.width {
-                let x: isize = x.try_into().unwrap();
-                let c = match self[(x, y)] {
+            for x in minx..=maxx {
+                let c = match self.get((x,y)) {
                     Cell::Empty => '.',
                     Cell::Rock => '#',
                     Cell::SandSource => '+',
@@ -202,7 +172,7 @@ pub mod p1 {
     use super::*;
 
     pub fn solve(input: &str) -> u32 {
-        let mut cave: Cave = input.parse().unwrap();
+        let mut cave: Cave<false> = input.parse().unwrap();
         let mut count = 0;
         while let Some(_pos) = cave.process_one_sand() {
             count += 1;
@@ -228,9 +198,7 @@ mod test {
     fn test_parse_cave() {
         let s = "498,4 -> 498,6 -> 496,6
             503,4 -> 502,4 -> 502,9 -> 494,9";
-        let cave: Cave = s.parse().unwrap();
-        assert_eq!(cave.width, 504);
-        assert_eq!(cave.height, 10);
+        let cave: Cave<false> = s.parse().unwrap();
         assert_eq!(
             format!("{cave}"),
             "\
@@ -251,7 +219,7 @@ mod test {
     fn test_process_sand() {
         let s = "498,4 -> 498,6 -> 496,6
             503,4 -> 502,4 -> 502,9 -> 494,9";
-        let mut cave: Cave = s.parse().unwrap();
+        let mut cave: Cave<false> = s.parse().unwrap();
         assert_eq!(cave.process_one_sand(), Some((500, 8)));
         assert_eq!(cave.process_one_sand(), Some((499, 8)));
         assert_eq!(cave.process_one_sand(), Some((501, 8)));
