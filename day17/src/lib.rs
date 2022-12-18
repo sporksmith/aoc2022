0291612cf8f1a2403391591bbdd492ed212015b7
Jim Newsome <jnewsome@torproject.org>
2022-12-18 00:30:22 -0600

    d17 p2 still broken

--- day17/src/lib.rs
+++ day17/src/lib.rs
@@ -140,110 +140,136 @@ impl Rock {
     }
 }
 
+fn collides_with_tower_or_wall(
+    shape: &Rock,
+    shape_pos: &Pos,
+    tower: &Rock,
+    tower_pos: &Pos,
+) -> bool {
+    if shape.minx + shape_pos.x <= 0 {
+        true
+    } else if shape.maxx + shape_pos.x > WIDTH {
+        true
+    } else if shape.miny + shape_pos.y == 0 {
+        true
+    } else {
+        shape.collides_with(shape_pos, tower, tower_pos)
+    }
+}
+
+fn print_tower(shape: &Rock, shape_pos: &Pos, tower: &Rock, tower_pos: &Pos) {
+    // Starting at y=0, which will be the *bottom*.
+    let mut lines = Vec::<String>::new();
+    for y in 1..=std::cmp::max(shape.maxy + shape_pos.y, tower.maxy + tower_pos.y) {
+        let mut line = String::new();
+        line.push('|');
+        for x in 1..=WIDTH {
+            let pos = Pos::new(x, y);
+            if shape.positions.contains(&(pos - *shape_pos)) {
+                line.push('@');
+            } else if tower.positions.contains(&(pos - *tower_pos)) {
+                line.push('#');
+            } else {
+                line.push('.');
+            }
+        }
+        line.push('|');
+        lines.push(line);
+    }
+    for line in lines.iter().rev() {
+        println!("{}", line);
+    }
+    println!(
+        "{}",
+        ['-']
+            .iter()
+            .cycle()
+            .take(WIDTH as usize + 2)
+            .collect::<String>()
+    )
+}
+
+pub fn simulate(input: &str, rock_limit: usize) -> isize {
+    let mut tower = Rock::new([Pos::new(0, 0); 0]);
+    let mut tower_pos = Pos::new(0, 0);
+
+    let mut shape = Rock::newi(0);
+    let mut shape_pos = Pos::new(LEFT_SPACE + 1, BOTTOM_SPACE + 1);
+
+    let mut jets = input.trim().chars().cycle();
+
+    let mut rocks_processed = 0;
+    //println!("Initial {:?} @ {:?}", shape, shape_pos);
+    //print_tower(&shape, &shape_pos, &tower, &tower_pos);
+    loop {
+        //println!("processed {rocks_processed}; current at {:?}", shape_pos);
+        let jet = jets.next().unwrap();
+        let jetted_pos = match jet {
+            '>' => shape_pos + Pos::new(1, 0),
+            '<' => shape_pos + Pos::new(-1, 0),
+            _ => panic!("unexpected c"),
+        };
+        if !collides_with_tower_or_wall(&shape, &jetted_pos, &tower, &tower_pos) {
+            //println!("Jet {jet} pushes");
+            shape_pos = jetted_pos;
+        } else {
+            //println!("Jet {jet} collides");
+        }
+        //print_tower(&shape, &shape_pos, &tower, &tower_pos);
+        let dropped_pos = shape_pos + Pos::new(0, -1);
+        if !collides_with_tower_or_wall(&shape, &dropped_pos, &tower, &tower_pos) {
+            shape_pos = dropped_pos;
+            //println!("Rock drops");
+            //print_tower(&shape, &shape_pos, &tower, &tower_pos);
+        } else {
+            // Combine with tower
+            tower.merge(&tower_pos, &shape, &shape_pos);
+
+            // Check if we can delete any lines.
+            // XXX this code isn't quite right, but also there's never a single complete
+            // line.
+            /*
+            for y in (shape_pos.y + shape.miny)..=(shape_pos.y + shape.maxy) {
+                if (1..=WIDTH).all(|x| tower.positions.contains(&Pos::new(x, y + tower_pos.y))) {
+                    tower_pos = Pos::new(tower_pos.x, tower_pos.y + y);
+                    tower.positions = tower
+                        .positions
+                        .iter()
+                        .copied()
+                        .filter(|p| p.y > y)
+                        .collect();
+                }
+            }
+            */
+
+            rocks_processed += 1;
+            // Spawn new shape
+            shape = Rock::newi(rocks_processed);
+            shape_pos = Pos::new(LEFT_SPACE + 1, tower.maxy + BOTTOM_SPACE + 1);
+
+            if rocks_processed % 100_000 == 0 {
+                println!(
+                    "Handled {rocks_processed} rocks; trimmed {} lines; {}% done",
+                    tower_pos.y,
+                    100.0 * rocks_processed as f32 / rock_limit as f32
+                );
+            }
+            if rocks_processed == rock_limit {
+                // println!("After {rocks_processed}");
+                //print_tower(&shape, &shape_pos, &tower, &tower_pos);
+                break;
+            }
+        }
+    }
+    println!("Final tower pos: {:?}", tower_pos);
+    print_tower(&shape, &shape_pos, &tower, &tower_pos);
+    tower.maxy
+}
 pub mod p1 {
     use super::*;
 
-    fn collides_with_tower_or_wall(
-        shape: &Rock,
-        shape_pos: &Pos,
-        tower: &Rock,
-        tower_pos: &Pos,
-    ) -> bool {
-        if shape.minx + shape_pos.x <= 0 {
-            true
-        } else if shape.maxx + shape_pos.x > WIDTH {
-            true
-        } else if shape.miny + shape_pos.y == 0 {
-            true
-        } else {
-            shape.collides_with(shape_pos, tower, tower_pos)
-        }
-    }
-
-    fn print_tower(shape: &Rock, shape_pos: &Pos, tower: &Rock, tower_pos: &Pos) {
-        // Starting at y=0, which will be the *bottom*.
-        let mut lines = Vec::<String>::new();
-        for y in 1..=std::cmp::max(shape.maxy + shape_pos.y, tower.maxy + shape_pos.y) {
-            let mut line = String::new();
-            line.push('|');
-            for x in 1..=WIDTH {
-                let pos = Pos::new(x, y);
-                if shape.positions.contains(&(pos - *shape_pos)) {
-                    line.push('@');
-                } else if tower.positions.contains(&(pos - *tower_pos)) {
-                    line.push('#');
-                } else {
-                    line.push('.');
-                }
-            }
-            line.push('|');
-            lines.push(line);
-        }
-        for line in lines.iter().rev() {
-            println!("{}", line);
-        }
-        println!(
-            "{}",
-            ['-']
-                .iter()
-                .cycle()
-                .take(WIDTH as usize + 2)
-                .collect::<String>()
-        )
-    }
-
     pub fn solve(input: &str) -> isize {
-        let mut tower = Rock::new([Pos::new(0, 0); 0]);
-        let tower_pos = Pos::new(0, 0);
-
-        let mut shape = Rock::newi(0);
-        let mut next_shape_idx = 1;
-        let mut shape_pos = Pos::new(LEFT_SPACE + 1, BOTTOM_SPACE + 1);
-
-        let mut jets = input.trim().chars().cycle();
-
-        let mut rocks_processed = 0;
-        //println!("Initial {:?} @ {:?}", shape, shape_pos);
-        //print_tower(&shape, &shape_pos, &tower, &tower_pos);
-        loop {
-            //println!("processed {rocks_processed}; current at {:?}", shape_pos);
-            let jet = jets.next().unwrap();
-            let jetted_pos = match jet {
-                '>' => shape_pos + Pos::new(1, 0),
-                '<' => shape_pos + Pos::new(-1, 0),
-                _ => panic!("unexpected c"),
-            };
-            if !collides_with_tower_or_wall(&shape, &jetted_pos, &tower, &tower_pos) {
-                //println!("Jet {jet} pushes");
-                shape_pos = jetted_pos;
-            } else {
-                //println!("Jet {jet} collides");
-            }
-            //print_tower(&shape, &shape_pos, &tower, &tower_pos);
-            let dropped_pos = shape_pos + Pos::new(0, -1);
-            if !collides_with_tower_or_wall(&shape, &dropped_pos, &tower, &tower_pos) {
-                shape_pos = dropped_pos;
-                //println!("Rock drops");
-                //print_tower(&shape, &shape_pos, &tower, &tower_pos);
-            } else {
-                // Combine with tower
-                tower.merge(&tower_pos, &shape, &shape_pos);
-
-                rocks_processed += 1;
-                if rocks_processed == 2022 {
-                    break;
-                } else {
-                    // Spawn new shape
-                    shape = Rock::newi(next_shape_idx);
-                    next_shape_idx += 1;
-                    shape_pos = Pos::new(LEFT_SPACE + 1, tower.maxy + BOTTOM_SPACE + 1);
-
-                    //println!("After {rocks_processed}");
-                    //print_tower(&shape, &shape_pos, &tower, &tower_pos);
-                }
-            }
-        }
-        tower.maxy
+        simulate(input, 2022)
     }
 }
 
@@ -251,7 +277,40 @@ pub mod p2 {
     use super::*;
 
     pub fn solve(input: &str) -> isize {
-        todo!()
+        let rocks = 1000000000000;
+        let min_cycle_len = input.len() as isize * 5;
+
+        let mut cycle_len = min_cycle_len;
+        loop {
+            let cycle_height = simulate(input, cycle_len as usize) as isize;
+            let cycle2_height = simulate(input, cycle_len as usize * 2) as isize;
+            let cycle_height2 = cycle_height * 2;
+            let diff = cycle_height2 - cycle2_height;
+            println!("cycle_len: {cycle_len}, diff: {diff}");
+            if diff == 0 {
+                break;
+            }
+            cycle_len += min_cycle_len;
+        }
+        let cycle_height = simulate(input, cycle_len as usize) as isize;
+
+        let cycle_height_2 = simulate(input, cycle_len as usize * 2) as isize;
+        println!(
+            "cycle height * 2: {}, (cycle*2) height: {}",
+            cycle_height * 2,
+            cycle_height_2
+        );
+
+        let complete_cycles = rocks / cycle_len;
+        println!("cycle len {cycle_len}; complete cycles {complete_cycles}");
+
+        let rem_cycles = rocks % cycle_len;
+
+        println!(
+            "cycle len {cycle_len}; complete cycles {complete_cycles} + {rem_cycles} remaining"
+        );
+
+        cycle_height * (rocks / cycle_len) // + simulate(input, rocks as usize % cycle_len as usize)
     }
 }
 
@@ -290,5 +349,11 @@ mod tests {
     fn test_solvep1() {
         let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
         assert_eq!(p1::solve(input), 3068);
+    }
+
+    #[test]
+    fn test_solvep2() {
+        let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>";
+        assert_eq!(p2::solve(input), 1514285714288);
     }
 }
